c017856a5f40d1e59d7ceb8b37cdd81f95bb0bbd
lupidschwarzneger <117677441+lupidschwarzneger@users.noreply.github.com>
2022-11-08 01:38:44 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 
+HEADERSIZE = 1000
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 2134))
 s.listen(5)
@@ -7,4 +9,9 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been estabilished!")
-    clientsocket.send(bytes("Welcome to this server", "utf-8"))
+
+    msg = "Welcome to the server!"
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+
+    clientsocket.send(bytes(msg, "utf-8"))
+    #clientsocket.close()
